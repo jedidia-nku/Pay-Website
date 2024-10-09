09fcc61c77e15e2cb88f823ffce8b0672740e82a
Igomigo <igomigofatai@gmail.com>
2024-10-09 12:08:56 +0100

    Update: server connected to mongodb database

--- Backend/app.js
+++ Backend/app.js
@@ -4,8 +4,13 @@ const morgan = require("morgan");
 const cors = require("cors");
 const dotenv = require("dotenv").config();
 
+const dbConnect = require("./config/mongoDbConfig");
+
 const app = express();
 PORT = process.env.PORT || 5050;
+
+// Import the database connection
+dbConnect();
 
 app.use(cors({
     origin: process.env.CLIENT_URL,
