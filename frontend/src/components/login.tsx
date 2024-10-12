eb6294cc23c44756595b04ebb3671d90a6dca8dc
Igomigo <igomigofatai@gmail.com>
2024-10-12 17:07:14 +0100

    Update: Login logic updated

--- frontend/src/components/login.tsx
+++ frontend/src/components/login.tsx
@@ -63,13 +63,8 @@ const LogIn: React.FC = () => {
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
-              {
-                  displayName && (
-                    <h1 className='text-xl font-bold m-0'>{`Welcome, ${displayName}`}</h1>
-                  )
-              }
             <h3 className="text-xl font-bold text-gray-900 md:text-2xl dark:text-white">
-              Log In to your account
+              {displayName ? `Welcome, ${displayName}` : "Log In to your account"}
             </h3>
             <form className="space-y-4 md:space-y-6" onSubmit={handleSubmit}>
               <div>
